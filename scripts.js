ea4e5a03e30c919889ccc2a193312bf88e196ef7
VALDEMIR ARAUJO ANDRADE <valdemiraraujo.github@gmail.com>
2025-04-04 21:54:00 -0300

    inseri os arquivos

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let circulo = document.querySelector(".circulo");
+let imagem = document.querySelector(".starbuks");
+
+function trocarCor(cor) {
+  circulo.style.background = cor;
+}
+
+function trocarImagem(img) {
+  imagem.src = img;
+}
